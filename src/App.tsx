ff8d79046a9b99dd4f43bb1d55af5c6e5e7c31d5
godeka <godeka@naver.com>
2025-06-23 13:25:22 +0900

    feat: characters 조회 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useQuery } from "@apollo/client";
+import { GET_CHARACTERS } from "./graphql/characters";
 import "./App.css";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface CharacterInfo {
+  id: number;
+  image: string;
+  name: string;
+  status: string;
+  gender: string;
+  species: string;
+  type: string;
+  created: string;
+}
 
 function App() {
+  const { data } = useQuery(GET_CHARACTERS, {
+    variables: { page: 1 },
+  });
+
   return (
     <div>
       <Tabs defaultValue="character" className="w-[400px]">
@@ -10,7 +34,19 @@ function App() {
           <TabsTrigger value="location">Location</TabsTrigger>
           <TabsTrigger value="episode">Episode</TabsTrigger>
         </TabsList>
-        <TabsContent value="character">Characters</TabsContent>
+        <TabsContent value="character">
+          {data?.characters.results.map((character: CharacterInfo) => (
+            <Card>
+              <CardHeader>
+                <CardTitle>{character.name}</CardTitle>
+                <CardDescription>{character.status}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <img src={character.image} alt={character.name} />
+              </CardContent>
+            </Card>
+          ))}
+        </TabsContent>
         <TabsContent value="location">Locations</TabsContent>
         <TabsContent value="episode">Episodes</TabsContent>
       </Tabs>
