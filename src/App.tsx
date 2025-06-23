d6d529d75f2c5d8a046d181223a904b19e54564d
godeka <godeka@naver.com>
2025-06-23 12:27:36 +0900

    feat: tabs 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Tabs defaultValue="character" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="character">Character</TabsTrigger>
+          <TabsTrigger value="location">Location</TabsTrigger>
+          <TabsTrigger value="episode">Episode</TabsTrigger>
+        </TabsList>
+        <TabsContent value="character">Characters</TabsContent>
+        <TabsContent value="location">Locations</TabsContent>
+        <TabsContent value="episode">Episodes</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default App;
