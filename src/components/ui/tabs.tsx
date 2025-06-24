d2b5402d41fcfadcfab13f15e1521951d6552390
godeka <godeka@naver.com>
2025-06-25 00:35:21 +0900

    fix: 캐릭터/로케이션/에피소드 탭 간 이동 시 상태 유지

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -41,7 +41,9 @@ const TabsContent = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.Content
     ref={ref}
+    forceMount // 추가
     className={cn(
+      "data-[state=inactive]:hidden", // 추가
       "pt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
       className
     )}
